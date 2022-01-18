a9065af5757261638a6039d25e22c53346968bda
Nandhini <62134452+nandysundram@users.noreply.github.com>
2022-01-18 16:17:17 +0530

    Add files via upload

---
+++ InterfaceModule/IUserMenu.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Application : online furniture Store
+ * AuthorName: Nandhini sundram
+ * Reviewed by :
+ * Created on : 03-Jan-2021
+ * Updated on : 12-Jan-2022
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayaFurnitureStore.InterfaceModule
+{
+     public interface UserMenu
+
+    {
+        void UserMenu();
+    }
+}
